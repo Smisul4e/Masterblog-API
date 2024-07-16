602f5112ab429b6120a906e1b33d793a25df712a
Smisul4e <teo.plamka@gmail.com>
2024-07-16 22:10:58 +0200

    Add basic blog post API with GET endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -2,18 +2,16 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)  # Това ще разреши CORS за всички маршрути
 
 POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
+    {"id": 1, "title": "First Post", "content": "This is the first post."},
+    {"id": 2, "title": "Second Post", "content": "This is the second post."},
 ]
-
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
